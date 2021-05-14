0ca3a28dbc3a44bfc02c6c7e650650a1c83e9c28
sebnem <63050857+sebnemgormus@users.noreply.github.com>
2021-05-14 19:29:13 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+class App extends React.Component {
+
+    state = {
+        movies: [
+            {
+                "id": 1,
+                "name": "The Flash",
+                "rating": 8.3,
+                "overview":"This is a wider card with supporting text below as a natural lead-in to additional content.",
+                "imageURL": "http://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
+            },
+            {
+                "id": 2,
+                "name": "Interstellar",
+                "rating": 6.8,
+                "overview":"This is a wider card with supporting text below as a natural lead-in to additional content.",
+                "imageURL": "http://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI61CU6Mx1NBvIx.jpg"
+            },
+            {
+                "id": 3,
+                "name": "Arrow",
+                "rating": 7.9,
+                "overview":"This is a wider card with supporting text below as a natural lead-in to additional content.",
+                "imageURL": "http://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div className = "container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <SearchBar />
+                    </div>
+                </div>
+                <MovieList />
+            </div>
+        )
+    }
+}
+
+export default App;
