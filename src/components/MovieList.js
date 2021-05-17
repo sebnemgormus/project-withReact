a738ed4a52b12da4cfb069d97b777ac1b2bf7875
sebnem <63050857+sebnemgormus@users.noreply.github.com>
2021-05-17 15:55:29 +0300

    added onClick Event

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const MovieList = (props) => {
 
+/*
+        function handleClick(event) {
+            console.log(event.pageX);
+        }
+
+ */
         return (
             <div className="row">
             {props.movies.map((movie) => (
+
                 <div className="col-lg-4" key={movie.id}>
                     <div className="card mb-4 shadow-sm">
                         <img src={movie.imageURL} className="card-img-top" alt="sample-movie"/>
@@ -12,7 +19,7 @@ const MovieList = (props) => {
                             <h5 className="card-title">{movie.name}</h5>
                             <p className="card-text">{movie.overview}</p>
                             <div className="d-flex justify-content-between align-items-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                <button type="button" onClick={(event) => props.deleteMovieProp(movie)} className="btn btn-md btn-outline-danger">Delete</button>
                                 <h2><span className="badge badge-info">{movie.rating}</span></h2>
                             </div>
                         </div>
