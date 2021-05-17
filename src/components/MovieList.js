41b1f3b74d2e6dbb03525a43136e885577e06d4c
sebnem <63050857+sebnemgormus@users.noreply.github.com>
2021-05-17 15:22:12 +0300

    update map function

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
-class MovieList extends React.Component {
-
-    render() {
+const MovieList = (props) => {
 
         return (
             <div className="row">
-                <div className="col-lg-4">
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
                     <div className="card mb-4 shadow-sm">
-                        <img src="http://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg" className="card-img-top" alt="sample-movie"/>
+                        <img src={movie.imageURL} className="card-img-top" alt="sample-movie"/>
                         <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="card-text">Sample Movie Description</p>
+                            <h5 className="card-title">{movie.name}</h5>
+                            <p className="card-text">{movie.overview}</p>
                             <div className="d-flex justify-content-between align-items-center">
                                 <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2><span className="badge badge-info">9.0</span></h2>
+                                <h2><span className="badge badge-info">{movie.rating}</span></h2>
                             </div>
                         </div>
                     </div>
                 </div>
+            )  )}
+
             </div>
         )
-
-    }
 }
 
 
