2f66478c006e07f1c03519f78af9cb7a43a9dc94
sebnem <63050857+sebnemgormus@users.noreply.github.com>
2021-05-25 18:40:26 +0300

    with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from "./AddMovie";
 import axios from 'axios';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 class App extends React.Component {
 
@@ -11,50 +13,11 @@ class App extends React.Component {
         searchQuery : ""
 
     }
-/*
-    async componentDidMount() {
-        const baseURL = "http://localhost:3002/movies";
-        const response = await fetch(baseURL);
-        console.log(response);
-        const data = await response.json();
-        this.setState({movies : data})
-    }
-*/
     async componentDidMount() {
         const response = await axios.get("http://localhost:3002/movies");
         this.setState({movies : response.data})
     }
-/*
-    deleteMovie = (movie) => {
-        const newMovieList  = this.state.movies.filter(
-            m => m.id !== movie.id
-        );
 
-        this.setState({
-            movies : newMovieList
-        })
-
-        this.setState(state =>({
-            movies : newMovieList
-        }))
-    }
-*/
-    /*
-//fetch api
-    deleteMovie = async (movie) => {
-        const baseURL = `http://localhost:3002/movies/${movie.id}`
-        await fetch(baseURL, {
-            method : "DELETE"
-        })
-        const newMovieList  = this.state.movies.filter(
-            m => m.id !== movie.id
-        );
-        this.setState(state =>({
-            movies : newMovieList
-        }))
-    }
-*/
-    //AXIOS
     deleteMovie = async (movie) => {
         axios.delete(`http://localhost:3002/movies/${movie.id}`)
         const newMovieList  = this.state.movies.filter(
@@ -64,7 +27,7 @@ class App extends React.Component {
             movies : newMovieList
         }))
     }
-//event onChange'den geliyor
+
     searchMovie = (event) => {
         //state updated
         this.setState({searchQuery : event.target.value})
@@ -79,19 +42,37 @@ class App extends React.Component {
         )
 
         return (
-            <div className = "container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <SearchBar searchMovieProp ={this.searchMovie}/>
-                    </div>
+            <Router>
+
+                <div className = "container">
+                <Switch>
+
+
+                    <Route path="/" exact render={() => (
+                        <React.Fragment>
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <SearchBar searchMovieProp ={this.searchMovie}/>
+                            </div>
+                        </div>
+
+                        <MovieList
+                            movies = {filteredMovies}
+                            deleteMovieProp = {this.deleteMovie}
+                        />
+                        </React.Fragment>
+                    )}>
+
+                    </Route>
+
+                    <Route path="/add" component={AddMovie}/>
+
+                </Switch>
                 </div>
-                <MovieList
-                    movies = {filteredMovies}
-                    deleteMovieProp = {this.deleteMovie}
-                />
-            </div>
+            </Router>
         )
     }
 }
 
 export default App;
+  
