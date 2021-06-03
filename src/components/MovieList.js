7b3c0fee9236cfb4f37181cc1d25a7ff3e921a66
sebnem <63050857+sebnemgormus@users.noreply.github.com>
2021-06-03 15:16:33 +0300

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieList = (props) => {
 
-/*
-        function handleClick(event) {
-            console.log(event.pageX);
+        const truncateOverview = (string, maxLenght) => {
+            if (!string) return null ; 
+            if (string.length <= maxLenght) return string;
+            return `${string.substring(0, maxLenght)} ...`;
         }
 
- */
         return (
             <div className="row">
             {props.movies.map((movie, i) => (
@@ -17,9 +18,15 @@ const MovieList = (props) => {
                         <img src={movie.imageURL} className="card-img-top" alt="sample-movie"/>
                         <div className="card-body">
                             <h5 className="card-title">{movie.name}</h5>
-                            <p className="card-text">{movie.overview}</p>
+                            <p className="card-text">{truncateOverview(movie.overview, 100)}</p>
                             <div className="d-flex justify-content-between align-items-center">
                                 <button type="button" onClick={(event) => props.deleteMovieProp(movie)} className="btn btn-md btn-outline-danger">Delete</button>
+
+                               <Link type = "button"
+                                     className = "btn btn-md btn-outline-primary"
+                                     to = {`edit/${movie.id}`}
+                               >Edit </Link>
+
                                 <h2><span className="badge badge-info">{movie.rating}</span></h2>
                             </div>
                         </div>
@@ -33,4 +40,3 @@ const MovieList = (props) => {
 
 
 export default MovieList;
-
